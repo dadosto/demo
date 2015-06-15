e6f92fe3afaa7c79ecc0aea96b76cb4f9413a781
Vladimir Stojov <vladimir.stojov@netcetera.com>
2015-06-15 19:37:43 +0200

    resolving conflict

--- GitDemo/src/main/java/hello/HelloWorld.java
+++ GitDemo/src/main/java/hello/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Change");
+		System.out.println("ChangeForConflict");
 		int a = 3;
 		System.out.println("a = " + a);
 	}
